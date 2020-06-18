9e0c65d625c8595f00c9d6e2dfc6d93e6d1cf2ef
leoxshn <leoxshn@gmail.com>
2020-06-18 18:57:28 +0200

    added options to apply iconpack to niagara and lawnchair launchers also added some icons

--- app/src/main/java/posidon/icons/hydra/Main.kt
+++ app/src/main/java/posidon/icons/hydra/Main.kt
@@ -1,14 +1,17 @@
 package posidon.icons.hydra
 
+import android.content.ActivityNotFoundException
 import android.content.ComponentName
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import posidon.icons.hydra.tools.Tools
 import java.lang.ref.WeakReference
+
 
 class Main : AppCompatActivity() {
 
@@ -20,12 +23,36 @@ class Main : AppCompatActivity() {
 
     fun applyPosidon(v: View) {
         if (isInstalled("posidon.launcher", packageManager)) {
-            val i = Intent(Intent.ACTION_MAIN)
-            i.component = ComponentName("posidon.launcher", "posidon.launcher.external.ApplyIcons")
-            i.putExtra("iconpack", BuildConfig.APPLICATION_ID)
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(i)
+            Intent(Intent.ACTION_MAIN).apply {
+                component = ComponentName("posidon.launcher", "posidon.launcher.external.ApplyIcons")
+                putExtra("iconpack", packageName)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(this)
+            }
         } else Toast.makeText(this, "Not installed", Toast.LENGTH_SHORT).show()
+    }
+
+    fun applyLawnchair(v: View) {
+        try {
+            Intent("ch.deletescape.lawnchair.APPLY_ICONS", null).apply {
+                putExtra("packageName", packageName)
+                startActivity(this)
+            }
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Not installed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun applyNiagara(v: View) {
+        try {
+            Intent("bitpit.launcher.APPLY_ICONS").apply {
+                `package` = "bitpit.launcher"
+                putExtra("packageName", packageName)
+                startActivity(this)
+            }
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Not installed", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun isInstalled(packageName: String, packageManager: PackageManager): Boolean {
@@ -38,7 +65,9 @@ class Main : AppCompatActivity() {
         startActivity(Intent(this, IconList::class.java))
     }
 
-    fun github(v: View) {
-
-    }
+    fun github(v: View) {try {
+        val uri = Uri.parse("https://github.com/leoxshn/hydra")
+        val i = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(i)
+    } catch (ignore: Exception) {} }
 }
